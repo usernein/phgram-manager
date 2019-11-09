63b788426314a6714d8d99200b500f9b44f2fe25
Cezar H <29507335+usernein@users.noreply.github.com>
2019-11-09 17:43:40 -0300

    test

--- manager.php
+++ manager.php
@@ -9,8 +9,8 @@ require 'phgram.phar';
 use \phgram\{Bot, BotErrorHandler, ArrayObj};
 use function \phgram\{ikb, show};
 Bot::closeConnection();
-define('PHM_VERSION', '1.3.17');
-define('PHM_DATE', '2019-11-09T17:40:01-03:00');
+define('PHM_VERSION', '1.3.18');
+define('PHM_DATE', '2019-11-09T17:43:20-03:00');
 # breakfile src/config.php
 
 $cfg = new stdClass();
